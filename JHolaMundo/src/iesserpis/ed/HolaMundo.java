dfcd6820a208705bf093837cc604a3ce70dea650
jorgejpl96 <jorlud@alu.iesserpis.org>
2019-09-30 14:00:30 +0200

    Añadido Carpeta JHolaMundo (holamundo en java con eclipse)

---
+++ JHolaMundo/src/iesserpis/ed/HolaMundo.java
@@ -0,0 +1,10 @@
+package iesserpis.ed;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo desde Eclipse");
+		System.out.println("2a linea escrita");
+	}
+
+}
