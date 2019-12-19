320f0667fb5e68315897da6bf4eb8bf0f27d4573
vigliom <jorgeperez96jpl@gmail.com>
2019-12-19 08:51:39 +0100

    Implementamos las comprobaciones de vector vacio tanto al orden de vectores como el maxima

--- JHolaMundo/src/iesserpis/ed/programa1.java
+++ JHolaMundo/src/iesserpis/ed/programa1.java
@@ -5,6 +5,10 @@ public class programa1 {
 	public static void main(String[] args) {
 		int max = max(new int[] { 4, 14, 24, 8, 7 });
 		System.out.println("mayor = " + max);
+		int[] vector = SelectionSort(new int[] { 4, 14, 24, 8, 7 });
+		for (int i = 0; i < vector.length; i++) {
+			System.out.print(vector[i] + ",");
+		}
 
 	}
 
@@ -17,4 +21,56 @@ public class programa1 {
 		return max;
 	}
 
+	public static int[] SelectionSort(int[] v) {
+		if(v.length == 0) {
+			throw new IllegalArgumentException();
+		}
+		int num;
+		for (int i = 0; i < v.length; i++) {
+			if (i != v.length - 1) {
+				for (int j = i; j < v.length; j++) {
+					if (v[j] < v[i]) {
+						num = v[i];
+						v[i] = v[j];
+						v[j] = num;
+					}
+				}
+
+			}
+		}
+
+		return v;
+	}
+
+	public static int max1(int[] v) {
+		if(v.length == 0) {
+			throw new IllegalArgumentException();
+		}
+		int max1= v[0];
+		for (int i = 1; i < v.length; i++) {
+			if (v[i] > max1) {
+				max1 = v[i];
+			}
+		}
+		return max1;
+
+	}
+	public static int[] OrdenParra(int[] v) {
+		int num;
+		for (int i = 0; i < v.length - 1; i++) {
+			if (v[i] > v[i + 1]) {
+
+				num = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = num;
+			}
+		}
+		if (v[v.length - 2] > v[v.length - 1]) {
+			num = v[v.length - 2];
+			v[v.length - 2] = v[v.length - 1];
+			v[v.length - 1] = num;
+		}
+
+		return v;
+	}
 }
--- JHolaMundo/src/iesserpis/ed/programa1Test.java
+++ JHolaMundo/src/iesserpis/ed/programa1Test.java
@@ -2,6 +2,9 @@ package iesserpis.ed;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class programa1Test {
@@ -9,6 +12,38 @@ class programa1Test {
 	@Test
 	void test() {
 		assertEquals(24, programa1.max(new int[] {4,14,24,8,7}));
+		assertEquals(24, programa1.max(new int[] {24,14,4,8,7}));
+		assertEquals(24, programa1.max(new int[] {4,24,14,8,7}));
+		assertEquals(24, programa1.max(new int[] {4,14,7,8,24}));
+		assertEquals(24, programa1.max(new int[] {4,14,8,24,7}));
+		assertEquals(24, programa1.max(new int[] {4,24,8,24,7}));
+		
+		//caso de vector vacío
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() ->programa1.max1(new int[] {})
+				
+			);
+
+
 	}
 
+	@Test
+	void test2() {
+		int[] v1 = { 14, 21, 12, 7, 9 };
+		programa1.SelectionSort(v1);
+		assertArrayEquals(new int[] { 7, 9, 12, 14, 21 }, v1);
+		
+		Assertions.assertThrows(IllegalArgumentException.class, 
+				() ->programa1.SelectionSort(new int[] {})
+				
+			);
+	}
+/*
+	@Test
+	void test3() {
+		int[] v1 = { 14, 21, 12, 7, 9 };
+		programa1.OrdenParra(v1);
+		assertArrayEquals(new int[] { 7, 9, 12, 14, 21 }, v1);
+	}
+*/
 }
