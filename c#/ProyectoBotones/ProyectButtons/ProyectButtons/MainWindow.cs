d375d26f234b84ae932fbd579410ce04bc9926bf
jorgejpl96 <jorlud@alu.iesserpis.org>
2020-01-27 13:52:50 +0100

    .

---
+++ c#/ProyectoBotones/ProyectButtons/ProyectButtons/MainWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private Random random = new Random();
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBSaludarClicked(object sender, EventArgs e)
+    {
+        MessageDialog md = new MessageDialog(null, DialogFlags.NoSeparator, MessageType.Warning, ButtonsType.Ok, "Hola, te estoy saludando" + Texto1.Text);
+        md.Run();
+        md.Destroy();
+    }
+
+    protected void OnBSalirClicked(object sender, EventArgs e)
+    {
+        this.Destroy();
+    }
+
+    protected void OnAleatorioClicked(object sender, EventArgs e)
+    {
+        int maxvalue = int.Parse(aleatorio1.Text);
+        int minvalue = int.Parse(aleatorio2.Text);
+        int indexAleatorio = random.Next(minvalue, maxvalue);
+        MessageDialog md = new MessageDialog(null, DialogFlags.Modal, MessageType.Question, ButtonsType.Close, "Número aleatorio " + indexAleatorio);
+        md.Run();
+        md.Destroy();
+    }
+}
