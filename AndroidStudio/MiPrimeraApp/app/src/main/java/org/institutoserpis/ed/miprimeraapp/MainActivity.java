277f87acfa9e797ac73741f407d0ac2c4f8827c9
vigliom <jorgeperez96jpl@gmail.com>
2020-03-09 13:34:19 +0100

    .

--- AndroidStudio/MiPrimeraApp/app/src/main/java/org/institutoserpis/ed/miprimeraapp/MainActivity.java
+++ AndroidStudio/MiPrimeraApp/app/src/main/java/org/institutoserpis/ed/miprimeraapp/MainActivity.java
@@ -1,7 +1,5 @@
 package org.institutoserpis.ed.miprimeraapp;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
